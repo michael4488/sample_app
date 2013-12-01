a2f2c7cbe1da03ab19630a737e75e5aefd955fde
michael <michael@simplee.com>
2013-12-01 10:30:49 +0200

    App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-  def handke_unverified_request
+  def handle_unverified_request
     sign_out
     super
   end
